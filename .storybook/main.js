81bf08672401ef4dfece3c6c06005fbe0a06d1ef
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-12-27 11:23:49 +0100

    add postcss to rollup config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -14,8 +12,7 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
+      use: ['style-loader', 'css-loader', 'sass-loader']
     });
     return config;
   }
