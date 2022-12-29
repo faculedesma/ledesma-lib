474fcfda7476d39f509145b79f403a46f9540349
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-12-29 18:04:56 +0100

    fix(progress): add missing ind export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as TableRow } from './Table/TableRow';
 export { default as TableHeaderCell } from './Table/TableHeaderCell';
 export { default as TableCell } from './Table/TableCell';
 export { default as Pagination } from './Pagination/Pagination';
+export { default as Indeterminate } from './Progress/Indeterminate';
